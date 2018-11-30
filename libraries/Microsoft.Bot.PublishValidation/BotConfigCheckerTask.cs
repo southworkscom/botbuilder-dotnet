0c778547149ae7b928ea6cc15e0649be76376017
JuanAr <juan.arguello@southworks.com>
2018-11-30 18:47:29 -0300

    Extract Task to command line Program

--- libraries/Microsoft.Bot.PublishValidation/BotConfigCheckerTask.cs
+++ libraries/Microsoft.Bot.PublishValidation/BotConfigCheckerTask.cs
@@ -1,15 +1,15 @@
-﻿using Microsoft.Build.Framework;
+﻿using Microsoft.Bot.Configuration;
 using TaskBuilder.Helpers;
 
 namespace Microsoft.Bot.PublishValidation
 {
-    public class BotConfigCheckerTask : Microsoft.Build.Utilities.Task
+    public class BotConfigCheckerTask
     {
         public string ProjectPath { get; set; }
 
-        public override bool Execute()
+        public bool Execute()
         {
-            LoggerHelper logHelper = new LoggerHelper(Log.LogError, Log.LogWarning);
+            //LoggerHelper logHelper = new LoggerHelper(Log.LogError, Log.LogWarning);
 
             string resultMsg = string.Empty;
             int logType = 0;
@@ -17,16 +17,18 @@ namespace Microsoft.Bot.PublishValidation
             // Validate if the Project directory path is valid
             if (!DirectoryValidatorHelper.DirectoryIsValid(ProjectPath, out resultMsg, out logType))
             {
-                logHelper.Log(resultMsg, logType);
+                //logHelper.Log(resultMsg, logType);
                 return false;
             }
 
             // Validate if there is any .bot file inside the Project Directory 
             if (!DirectoryValidatorHelper.FileExists(ProjectPath, "*.bot", out resultMsg, out logType))
             {
-                logHelper.Log(resultMsg, logType);
+                //logHelper.Log(resultMsg, logType);
                 return false;
             }
+
+            BotConfiguration bot = BotConfiguration.LoadFromFolder(ProjectPath);
 
             return true;
         }
---
+++ libraries/Microsoft.Bot.PublishValidation/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Microsoft.Bot.PublishValidation
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var task = new BotConfigCheckerTask();
+            task.ProjectPath = args[0];
+
+            try
+            {
+                if (!task.Execute())
+                {
+                    return 1;
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex);
+                return 2;
+            }
+
+            return 0;
+        }
+    }
+}
