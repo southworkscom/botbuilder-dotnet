59be69c05c32e0814b50b5f2ccf633347732e041
VictorGrycuk <victor@grycuk.net>
2019-03-20 10:38:39 -0300

    Move all functional code inside index.ts

--- source/index.ts
+++ source/index.ts
@@ -1,25 +1,74 @@
-import { configuration } from "./configuration";
-import { Core } from "./core";
-import tl from 'azure-pipelines-task-lib';
+import { join } from 'path'
+import { getInput, setResult, TaskResult } from 'azure-pipelines-task-lib';
+import { execSync } from "child_process";
+import { existsSync } from 'fs';
 
 run();
 
 function run(): void {
-    const config = new configuration();
-    const core: Core = new Core(config);
-    
+
+    // Create ApiCompat path
+    const ApiCompatPath = join(__dirname, 'ApiCompat', 'Microsoft.DotNet.ApiCompat.exe');
+
     // Get the binaries to compare
-    const inputFiles: string[] = core.getInputFiles();
+    const inputFiles: string = getInputFiles();
     
-    //const placeholder: string = config.parameters.packageSubFolder.value;
-    //const placeholder: string = join(__dirname, config.parameters.nugetPackageName.value);
-    
-    // run ApiCompat
-    //console.log(`"${config.ApiCompatPath}" "${inputFiles.join(',')}" --impl-dirs "${placeholder}"`);
-    const command = `"${config.ApiCompatPath}" "${inputFiles.join(',')}" --impl-dirs "${ tl.getInput('implFolder') }"`;
-    if (tl.getInput('failOnIssue') === 'true') {
-        core.runWithCustomError(command);
+    const command = `"${ ApiCompatPath }" "${ inputFiles }" --impl-dirs "${ getInput('implFolder') }"`;
+
+    if (getInput('failOnIssue') === 'true') {
+        runWithCustomError(command);
     } else {
-        core.runPlain(command);
+        runPlain(command);
     }
+}
+
+function getInputFiles(): string {
+    const filesName: string[] = [];
+    
+    getInput('contractsFileName').split(' ').forEach(file => {
+        const fullFilePath: string = join(getInput('contractsRootFolder'), file);
+        if (existsSync(fullFilePath)) {
+            filesName.push(fullFilePath);
+        }
+    });
+
+    return filesName.join(',');
+}
+
+function runPlain(command: string): string {
+    return execSync(command).toString();
+}
+
+function runWithCustomError(command: string): void{
+    command = addOptions(command);
+    try {
+        const result = execSync(command).toString();
+
+        if (parseMessage(result)) {
+            console.log(result);
+        }
+        else {
+            console.log(result);
+            LogError(`There were differences between the assemblies`)
+        }
+    }
+    catch (error) {
+        LogError(`A problem ocurred: ${error.message}`);
+    }
+}
+
+function parseMessage (message: string) {
+    return Number((message.split(':'))[1].trim()) === 0;
+}
+
+function addOptions(command: string): string{
+    command += getInput('resolveFx')? ' --resolve-fx' : '';
+    command += getInput('warnOnIncorrectVersion')? ' --warn-on-incorrect-version' : '';
+    command += getInput('warnOnMissingAssemblies')? ' --warn-on-missing-assemblies' : '';
+
+    return command;
+}
+
+function LogError(message: string): void {
+    setResult(TaskResult.Failed, message);
 }
