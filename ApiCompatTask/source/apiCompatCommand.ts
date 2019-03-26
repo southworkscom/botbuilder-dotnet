b675e64cc45e049c978519ec7328ae173462ef79
aliandi <mica.andini@gmail.com>
2019-03-26 09:38:07 -0300

    add option for baseline file

--- ApiCompatTask/source/apiCompatCommand.ts
+++ ApiCompatTask/source/apiCompatCommand.ts
@@ -27,6 +27,7 @@ export default class ApiCompatCommand {
         command = getInput('resolveFx') === 'true' ? ' --resolve-fx' : '';
         command += getInput('warnOnIncorrectVersion') === 'true' ? ' --warn-on-incorrect-version' : '';
         command += getInput('warnOnMissingAssemblies') === 'true' ? ' --warn-on-missing-assemblies' : '';
+        command += getInput('baselineFile') !== '' ? ' -b ' + getInput('baselineFile') : '';
         
         return command;
     }
