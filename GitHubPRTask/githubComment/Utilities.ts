010f4729509af433f38d5272163ebb723efdf172
Tomas Guzman <42191764+ParadoxARG@users.noreply.github.com>
2019-04-04 15:53:44 -0300

    Cleanup and set default values

--- GitHubPRTask/githubComment/Utilities.ts
+++
@@ -1,4 +0,0 @@
-export class AzureDevOpsVariables {
-    public static readonly buildRepositoryId: string = "Build.Repository.Id"; 
-    public static readonly pullRequestNumber: string = "System.PullRequest.PullRequestNumber"
-}
