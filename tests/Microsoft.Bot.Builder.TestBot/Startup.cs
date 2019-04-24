e98004cec13c6cdcb8a3328fe0d4d2399288af1f
Cecilia Avila <cecilia.avila@southworks.com>
2019-04-24 11:49:36 -0300

    Add TestBot.zip

--- tests/Microsoft.Bot.Builder.TestBot/Startup.cs
+++ tests/Microsoft.Bot.Builder.TestBot/Startup.cs
@@ -1,12 +1,14 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.BotFramework;
 using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Bot.Builder.TestBot.Bots;
 using Microsoft.Bot.Connector.Authentication;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -14,9 +16,13 @@ namespace Microsoft.BotBuilderSamples
 {
     public class Startup
     {
+        private string chosenBot = string.Empty;
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            GetEnvironmentVariables();
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             // Create the credential provider to be used with the Bot Framework Adapter.
@@ -38,7 +44,20 @@ namespace Microsoft.BotBuilderSamples
             services.AddSingleton<MainDialog>();
 
             // Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
-            services.AddTransient<IBot, DialogAndWelcomeBot<MainDialog>>();
+            switch (chosenBot)
+            {
+                case "MyBot":
+                    services.AddTransient<IBot, MyBot>();
+                    break;
+
+                case "DialogBot":
+                    services.AddTransient<IBot, DialogBot<MainDialog>>();
+                    break;
+
+                default:
+                    services.AddTransient<IBot, DialogAndWelcomeBot<MainDialog>>();
+                    break;
+            }
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -58,6 +77,19 @@ namespace Microsoft.BotBuilderSamples
 
             // app.UseHttpsRedirection();
             app.UseMvc();
+        }
+
+        public void GetEnvironmentVariables()
+        {
+            if (string.IsNullOrWhiteSpace(chosenBot))
+            {
+                chosenBot = Environment.GetEnvironmentVariable("CHOSENBOT");
+                if (string.IsNullOrWhiteSpace(chosenBot))
+                {
+                    // chosenBot = "DialogAndWelcomeBot";
+                    chosenBot = "MyBot";
+                }
+            }
         }
     }
 }
