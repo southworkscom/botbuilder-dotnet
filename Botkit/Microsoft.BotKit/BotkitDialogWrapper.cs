ebfa9808d47ec01b7cfcbd570a28ae3edd904619
Mai <mailen.havela@southworks.com>
2019-06-18 12:44:50 -0300

    Fix stylecop on the BotKitDialogWrapper file

--- Botkit/Microsoft.BotKit/BotkitDialogWrapper.cs
+++ Botkit/Microsoft.BotKit/BotkitDialogWrapper.cs
@@ -1,39 +1,45 @@
-// Copyright(c) Microsoft Corporation.All rights reserved.
-// Licensed under the MIT License.
-
-using Microsoft.BotKit.Conversation;
-using Microsoft.Bot.Builder.Dialogs;
-using System;
+﻿// <copyright file="BotkitDialogWrapper.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace Microsoft.BotKit
 {
+    using System;
+    using Microsoft.Bot.Builder.Dialogs;
+    using Microsoft.BotKit.Conversation;
+
+    /// <summary>
+    ///  BotkitDialogWrapper class.
+    /// </summary>
     public class BotkitDialogWrapper
     {
         /// <summary>
-        /// An object containing variables and user responses from this conversation.
+        /// Initializes a new instance of the <see cref="BotkitDialogWrapper"/> class.
         /// </summary>
-        public Tuple<string, object> Vars { get; set; }
-
+        /// <param name="dialogContext">dialogContext for the BotkitDialogWrapper.</param>
+        /// <param name="botkitconvoStep">botkitconvoStep for the BotkitDialogWrapper.</param>
         public BotkitDialogWrapper(DialogContext dialogContext, IBotkitConversationStep botkitconvoStep)
         {
-
         }
+
+        /// <summary>
+        /// Gets or sets an object containing variables and user responses from this conversation.
+        /// </summary>
+        public Tuple<string, object> Vars { get; set; }
 
         /// <summary>
         /// Jump immediately to the first message in a different thread.
         /// </summary>
-        /// <param name="thread">Name of a thread</param>
-        public async void GotoThread(string thread)
+        /// <param name="thread">Name of a thread.</param>
+        public void GotoThread(string thread)
         {
-
         }
 
         /// <summary>
         ///  Repeat the last message sent on the next turn.
         /// </summary>
-        public async void Repeat()
+        public void Repeat()
         {
-
         }
 
         /// <summary>
@@ -41,11 +47,10 @@ namespace Microsoft.BotKit
         /// Equivalent to convo.vars.key = val;
         /// Results in {{vars.key}} being replaced with the value in val.
         /// </summary>
-        /// <param name="key">The name of the variable</param>
-        /// <param name="val">The value for the variable</param>
+        /// <param name="key">The name of the variable.</param>
+        /// <param name="val">The value for the variable.</param>
         public void SetVar(object key, object val)
         {
-
         }
     }
 }
