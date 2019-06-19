8f7bb988c06967410b16189f7d0b38619fe64f08
Cecilia Avila <cecilia.avila@southworks.com>
2019-06-19 11:29:27 -0300

    Add SlackResponse auxiliary class

---
+++ Botkit/Microsoft.BotKit.Adapters.Slack/SlackResponse.cs
@@ -0,0 +1,13 @@
+﻿// Copyright(c) Microsoft Corporation.All rights reserved.
+// Licensed under the MIT License.
+
+namespace Microsoft.BotKit.Adapters.Slack
+{
+    public class SlackResponse
+    {
+        public bool ok;
+        public string channel;
+        public string ts;
+        public SlackMessage message;
+    }
+}
