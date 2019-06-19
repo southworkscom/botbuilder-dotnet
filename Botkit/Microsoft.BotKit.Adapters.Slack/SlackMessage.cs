38274612cdee1aa1c4871cc475551ac1a86ccd7b
Cecilia Avila <cecilia.avila@southworks.com>
2019-06-19 11:29:13 -0300

    Add SlackMessage auxiliary class

---
+++ Botkit/Microsoft.BotKit.Adapters.Slack/SlackMessage.cs
@@ -0,0 +1,16 @@
+﻿// Copyright(c) Microsoft Corporation.All rights reserved.
+// Licensed under the MIT License.
+
+namespace Microsoft.BotKit.Adapters.Slack
+{
+    public class SlackMessage
+    {
+        public string type;
+        public string subtype;
+        public string text;
+        public string ts;
+        public string username;
+        public string bot_id;
+        public string thread_ts;
+    }
+}
