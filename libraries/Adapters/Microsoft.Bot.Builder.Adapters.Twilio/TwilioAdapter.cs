d9c212bfc20399ca44c6cbb6dd24439c850c4095
VictorGrycuk <victor@grycuk.net>
2019-08-06 11:25:08 -0300

    Create static helper class

--- libraries/Adapters/Microsoft.Bot.Builder.Adapters.Twilio/TwilioAdapter.cs
+++ libraries/Adapters/Microsoft.Bot.Builder.Adapters.Twilio/TwilioAdapter.cs
@@ -173,30 +173,6 @@ namespace Microsoft.Bot.Builder.Adapters.Twilio
         }
 
         /// <summary>
-        /// Extracts attachments (if any) from a twilio message and returns them in an Attachments array.
-        /// </summary>
-        /// <param name="message">The TwilioEvent message.</param>
-        /// <returns>An Attachments array with the converted attachments.</returns>
-        private static List<Attachment> GetMessageAttachments(TwilioEvent message)
-        {
-            var attachments = new List<Attachment>();
-            if (int.TryParse(message.NumMedia, out var numMediaResult) && numMediaResult > 0)
-            {
-                for (var i = 0; i < numMediaResult; i++)
-                {
-                    var attachment = new Attachment()
-                    {
-                        ContentType = message.MediaContentTypes[i],
-                        ContentUrl = message.MediaUrls[i].AbsolutePath,
-                    };
-                    attachments.Add(attachment);
-                }
-            }
-
-            return attachments;
-        }
-
-        /// <summary>
         /// Converts a query string to a dictionary with key-value pairs.
         /// </summary>
         /// <param name="query">The query string to convert.</param>
@@ -293,7 +269,7 @@ namespace Microsoft.Bot.Builder.Adapters.Twilio
                 Text = twilioEvent.Body,
                 ChannelData = twilioEvent,
                 Type = ActivityTypes.Message,
-                Attachments = GetMessageAttachments(twilioEvent),
+                Attachments = TwilioHelper.GetMessageAttachments(twilioEvent),
             };
         }
     }
---
+++ libraries/Adapters/Microsoft.Bot.Builder.Adapters.Twilio/TwilioHelper.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Microsoft.Bot.Schema;
+
+namespace Microsoft.Bot.Builder.Adapters.Twilio
+{
+    public static class TwilioHelper
+    {
+        /// <summary>
+        /// Extracts attachments (if any) from a twilio message and returns them in an Attachments array.
+        /// </summary>
+        /// <param name="message">The TwilioEvent message.</param>
+        /// <returns>An Attachments array with the converted attachments.</returns>
+        public static List<Attachment> GetMessageAttachments(TwilioEvent message)
+        {
+            var attachments = new List<Attachment>();
+            if (int.TryParse(message.NumMedia, out var numMediaResult) && numMediaResult > 0)
+            {
+                for (var i = 0; i < numMediaResult; i++)
+                {
+                    var attachment = new Attachment()
+                    {
+                        ContentType = message.MediaContentTypes[i],
+                        ContentUrl = message.MediaUrls[i].AbsolutePath,
+                    };
+                    attachments.Add(attachment);
+                }
+            }
+
+            return attachments;
+        }
+    }
+}
