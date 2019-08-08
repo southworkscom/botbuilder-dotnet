760015851039463597914e405fa8752974df003e
VictorGrycuk <victor@grycuk.net>
2019-08-08 12:05:02 -0300

    Create TwilioApi class

---
+++ libraries/Adapters/Microsoft.Bot.Builder.Adapters.Twilio/TwilioAPI.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+namespace Microsoft.Bot.Builder.Adapters.Twilio
+{
+    public class TwilioAPI : ITwilioApi
+    {
+        public void LogIn(string username, string password)
+        {
+            TwilioClient.Init(username, password);
+        }
+
+        public async Task<object> CreateMessageResourceAsync(object messageOptions)
+        {
+            return await MessageResource.CreateAsync((CreateMessageOptions)messageOptions).ConfigureAwait(false);
+        }
+    }
+}
