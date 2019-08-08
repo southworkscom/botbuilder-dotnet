bc690eacdbb0b54fdc6c3b84c5e0e2d617639b5d
VictorGrycuk <victor@grycuk.net>
2019-08-08 12:05:51 -0300

    Create ITwilioApi interface

---
+++ libraries/Adapters/Microsoft.Bot.Builder.Adapters.Twilio/ITwilioAPI.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Bot.Builder.Adapters.Twilio
+{
+    public interface ITwilioApi
+    {
+        void LogIn(string username, string password);
+
+        Task<object> CreateMessageResourceAsync(object messageOptions);
+    }
+}
